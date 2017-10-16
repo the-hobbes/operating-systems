adb509665361b23df317ae6d845e60719863d659
Phelan Vendeville <phelan.vendeville@gmail.com>
2017-10-15 20:39:30 -0700

    Create writer function.
    Implement the "writers" function to coordinate writing across threads to the
    shared variable.

--- problem_set_1/priority.c
+++ problem_set_1/priority.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
+#include <unistd.h>
 
 /*
   Write a multi-threaded C program that gives readers priority over writers
@@ -24,7 +25,10 @@
 */
 
 #define NUM_THREADS 5
+int NUM_READERS = 0;
+int NUM_WRITERS = 0;
 int SHARED_VARIABLE = 0;
+int reader_has_priority = 0;
 pthread_mutex_t m = PTHREAD_MUTEX_INITIALIZER;
 pthread_cond_t write_ok = PTHREAD_COND_INITIALIZER;
 pthread_cond_t read_ok = PTHREAD_COND_INITIALIZER;
@@ -55,9 +59,32 @@ int main (void) {
 }
 
 void *writer (void *param) {
+  // remember that writers can only write when there are no readers
+  ++NUM_WRITERS;
+  // wait a random amount of time
+  int r = rand() % 6;
+  // ssssleep
+  fprintf(stdout, "Sleep for %d seconds\n", r);
+  sleep(r);
+  // grab the lock
+  pthread_mutex_lock(&m);
+  // are there any readers reading from the shared variable? 
+  if (reader_has_priority == 1) {
+    fprintf(stdout, "Reader must have priority");
+    pthread_cond_wait(&write_ok, &m);
+  }
+  // write and print the value
+  SHARED_VARIABLE = r;
+  printf("Variable value is: %d\n", r);
+  pthread_mutex_unlock(&m);
+  --NUM_WRITERS;
+  // print the number of readers present when writing (should be 0)
+  printf("Number of readers is: %d\n", NUM_READERS);
   return 0;
 }
 
 void *reader (void *param) {
+  ++NUM_READERS;
+  --NUM_READERS;
   return 0;
 }
